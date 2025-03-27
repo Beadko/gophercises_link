92d91a15b7a9fe9b12fd409a80b34dc7391ee4d3
Beadko <beadko@gmail.com>
2025-03-27 16:58:14 +0700

    Final tweaks

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/Beadko/gophercizes_link/link"
+)
+
+var (
+	ex = flag.String("example", "ex1.html", "Example file")
+)
+
+func main() {
+	flag.Parse()
+	f, err := os.ReadFile(*ex)
+	if err != nil {
+		fmt.Printf("Could not open file %s:%v", *ex, err)
+	}
+	links, err := link.Parse(bytes.NewReader(f))
+	if err != nil {
+		fmt.Printf("Could not parse the links from HTML %s:%v", *ex, err)
+		return
+	}
+	fmt.Printf("%+v\n", links)
+}
